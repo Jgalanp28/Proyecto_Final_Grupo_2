c3a342ff2421750d05da9f75c607d231ce33dfa4
amav27 <ana.acosta3@est.uexternado.edu.co>
2025-11-20 23:32:37 +0000

    Creacion Funcion Desviacion Estandar

--- pandas/Funciones.py
+++ pandas/Funciones.py
@@ -10,4 +10,24 @@ def promedios(nombre_archivo, nombre_columna):
         return promedio
     except Exception as e:
         print("Error al procesar la columna")
+    
         return None
+    
+def desviacion(nombre_archivo , nombre_columna):
+    try : 
+        df = pd.read_csv(nombre_archivo)
+    
+        if nombre_columna not in df.columns:
+            print("Error al procesar la columna")
+            return None
+    
+        desviacion_std = df[nombre_columna].std()
+        return desviacion_std
+
+    except Exception as e :
+        print("Error al procesar la columna")
+    
+        return None 
+
+
+
