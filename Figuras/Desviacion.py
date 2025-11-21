06ae4b98de0c80a95842517f5ec3e5147db4d018
amav27 <ana.acosta3@est.uexternado.edu.co>
2025-11-21 14:52:36 +0000

    Creacion Grafica Des

---
+++ Figuras/Desviacion.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def graficar_desviaciones(nombre_archivo):
+    try:
+        df = pd.read_csv(nombre_archivo)
+        columnas = ["CP1", "CP2", "CP3", "NF"]
+
+        for col in columnas:
+            if col not in df.columns:
+                print(f"Columna no encontrada: {col}")
+                return
+
+        desviaciones = df[columnas].std()
+
+        plt.bar(desviaciones.index, desviaciones.values)
+        plt.xlabel("Categorías")
+        plt.ylabel("Desviación Estándar")
+        plt.title("Desviaciones Estándar: CP1, CP2, CP3 y NF")
+        plt.tight_layout()
+
+        plt.savefig("Figuras/Desviaciones.png")
+
+        plt.show()
+
+    except Exception as e:
+        print("Error al procesar el archivo:", e)
+
+
+graficar_desviaciones("Quiz Cálculo 1- Gobierno 22 de Octubre(1-18).csv")
